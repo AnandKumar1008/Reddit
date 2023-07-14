19e535fc21ab9eda356467945123e4bd9ea34f80
AnandKumar <anandkumar031199@gmail.com>
2023-07-14 23:43:00 +0530

    every thing is added

--- src/Page/Comingpage/Comingpage.jsx
+++ src/Page/Comingpage/Comingpage.jsx
@@ -3,16 +3,20 @@ import "./Comingpage.css";
 import Nav from "../../Components/Nav/Nav";
 import Menu from "../../Components/Menu/Menu";
 import { MyContext } from "../../App";
+import coming from "../../Components/Image/c.webp";
 const Comingpage = () => {
-  const { showForm, over_lay } = useContext(MyContext);
+  const { showForm, over_lay, menu } = useContext(MyContext);
   return (
     <div
       className="reddit_clone_comimg_page"
       style={showForm == "none" ? {} : over_lay}
     >
-      <Menu />
-      <p>Stay Tuned</p>
-      <p>Page is Under Construction</p>
+      {menu && (
+        <div className="reddit_clone-coming_page_pseudo">
+          <Menu />
+        </div>
+      )}
+      <img src={coming} alt="" />
       <Nav />
     </div>
   );
